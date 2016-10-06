41ae6563fcc4312ec8b1f2e6dcea376674182f9f
lkcz04185 <lkcz04185@gmail.com>
2016-10-06 13:18:08 +0000

    user permissions for edit and update

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,5 @@
 class PlacesController < ApplicationController
-before_action :authenticate_user!, only: [:new, :create]
+before_action :authenticate_user!, only: [:new, :create, :edit]
 
   def index
    # @places = Place.all
@@ -21,6 +21,9 @@ before_action :authenticate_user!, only: [:new, :create]
 
   def edit
      @place = Place.find(params[:id])
+     if @place.user != current_user
+      return render text: 'Not Allowed', status: :forbidden
+    end
   end
 
   def update

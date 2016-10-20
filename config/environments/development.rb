c5a00ef4626312f11c8fef85fb92702219d6bdf6
lkcz04185 <lkcz04185@gmail.com>
2016-10-20 14:34:20 +0000

    e-mail gets trigerred after comment

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,17 +16,17 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
 
-  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.delivery_method = :test
 
-  config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
-    port: 587,
-    domain: "example.com",
-    authentication: "plain",
-    enable_starttls_auto: true,
-    user_name: ENV['GMAIL_ADDRESS'],
-    password: ENV['GMAIL_PASSWORD']
-  }
+  # config.action_mailer.smtp_settings = {
+    # address: "smtp.gmail.com",
+    # port: 587,
+    # domain: "example.com",
+    # authentication: "plain",
+    # enable_starttls_auto: true,
+    # user_name: ENV['GMAIL_ADDRESS'],
+    # password: ENV['GMAIL_PASSWORD']
+  # }
 
   config.action_mailer.default_url_options = {host: 'localhost:3030'}
 
